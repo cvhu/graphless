049ecf02e453fda398b118b0e8b9b36271286a3d
cvhu <vic@cvhu.org>
2013-11-14 14:17:14 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphless::Application.config.secret_token = '6bec78086371d5ea04dc2b211237a8014abb6d227b4e1fe2afc805f1b822971e2b2fda579ffbe19b580141e3d5edef849c6412d6791666b16508120f7f08e6ae'
